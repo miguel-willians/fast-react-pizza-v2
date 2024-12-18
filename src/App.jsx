4e88efce0c7893ba4cf47fb8687d0072883e61ec
miguel-willians <miiguel.wso@gmail.com>
2024-12-18 10:19:27 -0300

    Adcionando rotas no App e criando o AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
 
+// Forma de declarar rotas na v6 do React Router:
 const router = createBrowserRouter([
   {
     path: "/",
@@ -11,6 +14,8 @@ const router = createBrowserRouter([
   },
   { path: "/menu", element: <Menu /> },
   { path: "/cart", element: <Cart /> },
+  { path: "/order/:orderId", element: <Order /> },
+  { path: "/order/new", element: <CreateOrder /> },
 ]);
 
 function App() {
