e510f934cf16d7d75d8dce332f732a52e44f3373
miguel-willians <miiguel.wso@gmail.com>
2024-12-18 10:05:54 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
