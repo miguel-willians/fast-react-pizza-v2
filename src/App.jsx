c5741edba05f9508ec3acb774f6dec05d3edfce8
miguel-willians <miiguel.wso@gmail.com>
2024-12-18 15:26:34 -0300

    Criando e implementando o searchOrder

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Error from "./ui/Error";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 
 // Forma de declarar rotas na v6 do React Router:
@@ -24,7 +24,12 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
       { path: "/order/new", element: <CreateOrder /> },
     ],
   },
