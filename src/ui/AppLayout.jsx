a6a2b2f345d02d6d7ec64af1f5d74ca4fec341a5
miguel-willians <miiguel.wso@gmail.com>
2024-12-18 14:55:46 -0300

    Criando e implementando o Loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  // useNavigation: Um hook que "monitora" se um componente foi ou não renderizado (state: idle ou loading)
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         {/* Responsável por renderizar o conteúdo das nested routes: */}
