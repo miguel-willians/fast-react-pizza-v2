dceb4f05ccd2827849053bc0fb220698d9c07285
miguel-willians <miiguel.wso@gmail.com>
2024-12-20 17:19:38 -0300

    Adicionando flex-box em alguns componentes

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">NAME</div>;
+  return <div className="hidden text-sm font-semibold md:block">NAME</div>;
 }
 
 export default Username;
