331b9ac50b4bc0dc5e3edf447acbba232646ce14
miguel-willians <miiguel.wso@gmail.com>
2024-12-21 13:28:06 -0300

    Estilizando os inputs em alguns componentes

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -64,7 +64,12 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              required
+              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:bg-yellow-400 focus:outline-none focus:ring md:px-6 md:py-3"
+            />
           </div>
         </div>
 
@@ -75,6 +80,7 @@ function CreateOrder() {
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
+            className="focus:outlin e-none h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2"
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
