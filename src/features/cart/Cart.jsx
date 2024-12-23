7edd244a0cacc1badd80fadbd30fa168662a9c32
miguel-willians <miiguel.wso@gmail.com>
2024-12-23 11:50:55 -0300

    Atualizando a página do Cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,38 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
+import { clearCart, getCart } from "./cartSlice";
+import { getUsername } from "../user/userSlice";
 
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import EmptyCart from "./EmptyCart";
 
 function Cart() {
-  const username = useSelector((state) => state.user.username);
+  const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
 
-  const cart = fakeCart;
+  const dispatch = useDispatch();
+
+  function handleClear() {
+    dispatch(clearCart());
+  }
+
+  console.log(cart);
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-3">
@@ -56,7 +44,9 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={handleClear}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
